5bcd00435804c49dafccbff6d650ba55cac884e8
mostafa <mostafaahmedmaa1@gmail.com>
2024-01-27 11:23:49 +0200

    update

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;  //Variable to track the connection status
+let isConnected = false;  // Variable to track the connection status
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
@@ -10,10 +10,7 @@ export const connectToDB = async () => {
     if(isConnected) return console.log('=> using existing database connection')
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI,
-            {
-                dbName: process.env.DATABASE
-            })
+        await mongoose.connect(process.env.MONGODB_URI)
 
         isConnected = true;
 
