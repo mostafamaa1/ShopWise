31f2382c1d33ef5f1802fcbac9f665e2b144956f
mostafa <mostafaahmedmaa1@gmail.com>
2024-07-31 16:42:50 +0300

    update Thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,12 @@ export const metadata: Metadata = {
     siteName: 'Shopwise',
     images: [
       {
-        url: 'https://shopwise-inky.vercel.app/shopwise1.png', // Must be an absolute URL
+        url: '../public/shopwise1.png', // Must be an absolute URL
         width: 800,
         height: 600,
       },
       {
-        url: 'https://shopwise-inky.vercel.app/shopwise1.png', // Must be an absolute URL
+        url: '../public/shopwise1.png', // Must be an absolute URL
         width: 1800,
         height: 1600,
         alt: '',
